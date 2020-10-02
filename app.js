6d2bae4afb47cc611974d784ef7d86d51b604263
g-mahiyodin <72243016+g-mahiyodin@users.noreply.github.com>
2020-10-01 22:47:52 -0700

    add a js file

---
+++ app.js
@@ -0,0 +1,26 @@
+// alert("this is js")
+// var hint = "please read it"
+// alert(hint)
+// var a = prompt("put your id","here")
+// if( a === "Ali"){
+//     alert("this is your name")
+// }
+// else{
+//     alert("Enter correct name")
+// }
+// var percent = +prompt("Enter your %","Enter here")
+// if( percent >= 80 && percent <= 100){
+//     alert("Your Grade is A+")
+// }
+// else if( percent >= 70 && percent <= 80){
+//     alert("Your Grade is A")
+// }
+// else if( percent >= 60 && percent <= 70){
+//     alert("Your Grade is B")
+// }
+// else if( percent < 60){
+//     alert("You are Fail!!!")
+// }
+// else{
+//     alert("invalid percent")
+// }
